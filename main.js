ba6c75b670ffaa503a2a2e375a76094f137785a5
vihaan962 <82108747+vihaan962@users.noreply.github.com>
2021-11-09 17:37:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function take_snapshot(){
 }
 
 console.log('ml5version5', ml5.version);
-classifier = ml5/imageClassifier('https://teachablemachine.withgoogle.com/models/OoRKJPVF2/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/OoRKJPVF2/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log('Model Loaded');
