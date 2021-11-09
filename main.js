2c461ee6c7f86d8550aeecbb3e66b8609cc6402c
vihaan962 <82108747+vihaan962@users.noreply.github.com>
2021-11-09 17:36:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ camera = document.getElementById("camera");
 Webcam.attach('#camera');
 
 function take_snapshot(){
-    Webcam.snap(function(data_url){
+    Webcam.snap(function(data_uri){
         document.getElementById("result").innerHTML = '<img id="captured_image"src="'+data_uri+'"/>'; 
     })
 }
